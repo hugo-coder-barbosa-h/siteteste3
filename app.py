8406fa47f7d9be87e942c74350b4884f6619af4d
hugo-coder-barbosa-h <127802457+hugo-coder-barbosa-h@users.noreply.github.com>
2023-03-13 22:24:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
+app=Flask(_name_)
 
-app = Flask(__name__)
+menu="""
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> ! <a href="/contato">Contato</a>
+<br>
+"""
 
 @app.route("/")
-def index():
-  return "Olá, Esse é meu site (Hugo.H)"
+def hello_world():
+  return menu+"Olá, mundo! Esse é o meu site. (Graziela França)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da pag sobre"
+  return menu+"Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da pag Contato"
+  return menu+"Aqui vai o conteúdo da página Contato"
