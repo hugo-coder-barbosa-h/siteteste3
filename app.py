4c6d235d7ad9287b70579e317ab29e40c4d6afe0
hugo-coder-barbosa-h <127802457+hugo-coder-barbosa-h@users.noreply.github.com>
2023-03-13 21:50:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
   return "Olá, Esse é meu site (Hugo.H)"
+
+@app.route("/sobre")
+def sobre():
+  return "Aqui vai o conteúdo da pag sobre"
+
+@app.route("/contato")
+def contato():
+  return "Aqui vai o conteúdo da pag Contato"
