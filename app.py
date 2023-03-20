d8dfb961f9c9fff9210af11d24efd8a6e3484100
hugo-coder-barbosa-h <127802457+hugo-coder-barbosa-h@users.noreply.github.com>
2023-03-20 20:46:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,40 @@
 from flask import Flask
-app=Flask(_name_)
+from tchan import ChannelScraper
 
-menu="""
-<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> ! <a href="/contato">Contato</a>
+
+app = Flask(__name__)
+
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+    
+menu = """
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
-def hello_world():
-  return menu+"Olá, mundo! Esse é o meu site. (Graziela França)"
+def index():
+  return menu + "Olá, mundo! Esse é meu site. (Hugo H)"
 
 @app.route("/sobre")
 def sobre():
-  return menu+"Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return menu+"Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
+
+
 @app.route("/promocoes")
 def promocoes():
   conteudo = menu + """
