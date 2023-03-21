fa17bc98a846c6bfb9b42020b5eb1efb0f883d71
hugo-coder-barbosa-h <127802457+hugo-coder-barbosa-h@users.noreply.github.com>
2023-03-20 21:56:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,65 @@
-from flask import Flask
-app=Flask(_name_)
 
-menu="""
-<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> ! <a href="/contato">Contato</a>
+from flask import Flask
+from tchan import ChannelScraper
+
+app = Flask(__name__)
+
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+    
+menu = """
+<a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
 """
 
 @app.route("/")
 def index():
-  return "Olá, Esse é meu site (Hugo.H)"
-def hello_world():
-  return menu+"Olá, mundo! Esse é o meu site. (Graziela França)"
+  return menu + "Olá, mundo! Esse é meu site. (Álvaro Justen)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da pag sobre"
-  return menu+"Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da pag Contato"
-  return menu+"Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
+
+
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
+
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
