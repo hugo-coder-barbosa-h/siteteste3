5d2a9af8b42266b51d4d2e53b84ed6897a10e73d
hugo-coder-barbosa-h <127802457+hugo-coder-barbosa-h@users.noreply.github.com>
2023-03-20 21:09:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from tchan import ChannelScraper
 
 
 app = Flask(__name__)
@@ -48,18 +47,4 @@ def promocoes():
 
 
 @app.route("/promocoes2")
-def promocoes2():
-  conteudo = menu + """
-  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
-  <br>
-  <ul>
-  """
-  scraper = ChannelScraper()
-  contador = 0
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    conteudo += f"<li>{message.created_at} {texto}</li>"
-    if contador == 10:
-      break
-  return conteudo + "</ul>"
+
